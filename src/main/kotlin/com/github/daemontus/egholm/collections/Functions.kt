58c157724adf01a1a3cb2c76baa455cc70052eea
Samuel Pastva <daemontus@gmail.com>
2016-04-03 17:21:03 +0200

    Some utility functions for collections

---
+++ src/main/kotlin/com/github/daemontus/egholm/collections/Functions.kt
@@ -0,0 +1,27 @@
+package com.github.daemontus.egholm.collections
+
+/**
+ * Repeat given object specified number of times.
+ */
+fun <T> T.repeat(times: Int): Iterable<T> = object : Iterable<T> {
+
+    override fun iterator(): Iterator<T> = object : Iterator<T> {
+        var index = 0
+
+        override fun hasNext(): Boolean = index < times
+
+        override fun next(): T = this@repeat
+    }
+
+}
+
+/**
+ * Returns a list of given size with items initialised to given value
+ */
+fun <T> listWithInitial(size: Int, initial: T): List<T> = (1..size).map { initial }
+
+/**
+ * Same as array creation, but for lists
+ */
+fun <T> listWithInitial(size: Int, initializer: (Int) -> T): List<T> = (0 until size).map { initializer(it) }
+
