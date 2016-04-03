6d3d862882255ecff1461085e02bafc77ac4cee4
Samuel Pastva <daemontus@gmail.com>
2016-04-03 17:27:01 +0200

    lazy logging

---
+++ src/main/kotlin/com/github/daemontus/egholm/logger/LazyLogging.kt
@@ -0,0 +1,44 @@
+package com.github.daemontus.egholm.logger
+
+import java.util.logging.Level
+import java.util.logging.Logger
+
+/*
+ * Logger extensions for painless lazy logging
+ */
+
+inline fun Logger.lSevere(action: () -> String) {
+    if (this.isLoggable(Level.SEVERE)) {
+        this.severe(action())
+    }
+}
+
+inline fun Logger.lWarning(action: () -> String) {
+    if (this.isLoggable(Level.WARNING)) {
+        this.info(action())
+    }
+}
+
+inline fun Logger.lInfo(action: () -> String) {
+    if (this.isLoggable(Level.INFO)) {
+        this.info(action())
+    }
+}
+
+inline fun Logger.lFine(action: () -> String) {
+    if (this.isLoggable(Level.FINE)) {
+        this.fine(action())
+    }
+}
+
+inline fun Logger.lFiner(action: () -> String) {
+    if (this.isLoggable(Level.FINER)) {
+        this.fine(action())
+    }
+}
+
+inline fun Logger.lFinest(action: () -> String) {
+    if (this.isLoggable(Level.FINEST)) {
+        this.finest(action())
+    }
+}
