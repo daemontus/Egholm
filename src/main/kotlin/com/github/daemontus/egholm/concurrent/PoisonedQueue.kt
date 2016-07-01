b0ba660e5469fa65e04aff2bbd579728e9345504
Samuel Pastva <daemontus@gmail.com>
2016-07-01 16:24:37 +0200

    Remove Maybe

--- src/main/kotlin/com/github/daemontus/egholm/concurrent/PoisonedQueue.kt
+++ src/main/kotlin/com/github/daemontus/egholm/concurrent/PoisonedQueue.kt
@@ -1,11 +1,11 @@
 package com.github.daemontus.egholm.concurrent
 
-import com.github.daemontus.egholm.functional.Maybe
+import com.github.daemontus.egholm.functional.Option
 import com.github.daemontus.egholm.thread.GuardedThread
 import java.util.concurrent.BlockingQueue
 
 
-fun <T> BlockingQueue<Maybe<T>>.guardedThreadUntilPoisoned(onItem: (T) -> Unit): GuardedThread {
+fun <T> BlockingQueue<Option<T>>.guardedThreadUntilPoisoned(onItem: (T) -> Unit): GuardedThread {
     return GuardedThread(threadUntilPoisoned(onItem))
 }
 
@@ -13,10 +13,10 @@ fun <T> BlockingQueue<Maybe<T>>.guardedThreadUntilPoisoned(onItem: (T) -> Unit):
  * Create a thread listening to all items in a blocking queue until Nothing is received.
  * (Classic poison pill principle)
  */
-fun <T> BlockingQueue<Maybe<T>>.threadUntilPoisoned(onItem: (T) -> Unit): Thread {
+fun <T> BlockingQueue<Option<T>>.threadUntilPoisoned(onItem: (T) -> Unit): Thread {
     val result = Thread {
         var job = this.take()
-        while (job is Maybe.Just) {
+        while (job is Option.Some<T>) {
             onItem(job.value)
             job = this.take()
         }   //got Nothing
@@ -28,4 +28,4 @@ fun <T> BlockingQueue<Maybe<T>>.threadUntilPoisoned(onItem: (T) -> Unit): Thread
 /**
  * And poison it afterwards.
  */
-fun <T: Any> BlockingQueue<Maybe<T>>.poison() = this.put(Maybe.Nothing())
+fun <T: Any> BlockingQueue<Option<T>>.poison() = this.put(Option.None<T>())
